bea3c65bd81c36d7c3ed3aeb23c7d4e7a945b692
nandabi <valerie.nandabi@student.moringaschool.com>
2024-03-03 17:24:01 +0300

    commit message

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print("[]")
+    else:
+        fibonacci_sequence = [0, 1] if length > 1 else [0]
+        while len(fibonacci_sequence) < length:
+            next_fibonacci = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_fibonacci)
+        print(f"[{', '.join(map(str, fibonacci_sequence))}]")       
